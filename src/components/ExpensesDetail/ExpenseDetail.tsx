24df902f9468f0b9140a9919047a1d652f2cdcca
j-novaes <joaovictor.nb21@hotmail.com>
2022-02-27 16:31:31 +0000

    Edited ExpenseDetail

--- src/components/ExpensesDetail/ExpenseDetail.tsx
+++ src/components/ExpensesDetail/ExpenseDetail.tsx
@@ -1,52 +1,27 @@
-import { useContext } from "react";
+//Style
+import './ExpensesDetail.style.css'
 
-//Context
-import { GlobalContext } from "../../context/GlobalState";
-
-export const CategoryPercentage = (props:any) => {
-    const { transactions } = useContext(GlobalContext);
-
-    function getTotalByCategory(category: string) {
-        if (transactions.length <= 0) return 0;
-        let total = 0;
-        let filteredAmounts = transactions.filter((transaction: any) => transaction.category === category).map((transaction: any) => transaction.amount)
-    
-        if (filteredAmounts.length <= 0) return 0;
-    
-        total = filteredAmounts.reduce((total: any, amount: any) => total + amount);
-        return total;
-    }
-    
-    function getExpenseTotal() {
-        let amount = transactions.map((t: any) => t.amount).filter((a: any) => a < 0);
-        if (amount.length == 0) return 1;
-        let expenseTotal = amount.reduce((t: number, n: number) => t + n);
-    
-        return expenseTotal;
-    }
-
-    const {category} = props
-    return <progress 
-                value={Math.abs(getTotalByCategory(category))}
-                max={Math.abs(getExpenseTotal())}
-            ></progress>
-}
+//Components
+import CategoryPercentageBar from "../CategoryPercentageBar/CategoryPercentageBar";
 
 const ExpenseDetail = () => {
 
-    //const { transactions } = useContext(GlobalContext);
-
-
     return (
-        <div>
-            <h3>Details</h3>
+        <details>
+            <summary>
+                <h3>Details</h3>
+            </summary>
 
-            <CategoryPercentage category='food'/>
-            <CategoryPercentage category='home'/>
-            <CategoryPercentage category='education'/>
-            <CategoryPercentage category='vehicle'/>
-            <CategoryPercentage category='other'/>
-        </div>
+            <CategoryPercentageBar title='food:' category='food' />
+
+            <CategoryPercentageBar title='education:' category='education' />
+
+            <CategoryPercentageBar title='vehicle:' category='vehicle' />
+
+            <CategoryPercentageBar title='home:' category='home' />
+            
+            <CategoryPercentageBar title='other:' category='other' />
+        </details>
     )
 }
 
